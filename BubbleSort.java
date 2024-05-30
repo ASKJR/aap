568860748008dffe119c53636d1dae7b1858abc8
Alberto Kato <albertokatojr@gmail.com>
2024-05-30 15:06:14 -0300

    1) Minimizar Parâmetros

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,53 +1,67 @@
-// Implementação otimizada em Java do Bubble sort
-// Código extraído de https://www.geeksforgeeks.org/bubble-sort/
 
+/*
+* Aluno: Alberto Sussumu Kato Junior
+* 
+
+/*
+* ENUNCIADO:
+* Faça uma versão clean code do arquivo anexo. 
+* O arquivo anexado é um exemplo de código Bubble Sort. 
+* Faça ao menos 3 alterações no arquivo para ser mais "clean code" e 
+* explique quais alterações você fez. 
+* Caso você não queira utilizar este código pode escolher outro 
+* em Sorting Algorithms - GeeksforGeeks
+*/
+
+/*
+ * MODIFICAÇÕES:
+ * 1) Minimizar Parâmetros: Remoção do parâmetro tamanho de array (n) para os
+ * métodos: bubbleSort e printArray.
+ * 
+ */
 import java.io.*;
 
 class BubbleSort {
 
-    // An optimized version of Bubble Sort
-    static void bubbleSort(int arr[], int n)
-    {
-        int i, j, temp;
-        boolean trocado;
-        for (i = 0; i < n - 1; i++) {
-            trocado = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+  // An optimized version of Bubble Sort
+  public static void bubbleSort(int arr[]) {
+    int i, j, temp;
+    boolean trocado;
+    for (i = 0; i < arr.length - 1; i++) {
+      trocado = false;
+      for (j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
 
-                    // Swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    trocado = true;
-                }
-            }
-
-            // If no two elements were
-            // trocado by inner loop, then break
-            if (trocado == false)
-                break;
+          // Swap arr[j] and arr[j+1]
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+          trocado = true;
         }
-    }
+      }
 
-    // Function to print an array
-    static void printArray(int arr[], int size)
-    {
-        int i;
-        for (i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+      // If no two elements were
+      // trocado by inner loop, then break
+      if (trocado == false)
+        break;
     }
+  }
 
-    // Driver program
-    public static void main(String args[])
-    {
-        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-        int n = arr.length;
-        bubbleSort(arr, n);
-        System.out.println("Array ordenado: ");
-        printArray(arr, n);
-    }
+  // Function to print an array
+  static void printArray(int arr[]) {
+    int i;
+    for (i = 0; i < arr.length; i++)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+  }
+
+  // Driver program
+  public static void main(String args[]) {
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    bubbleSort(arr);
+    System.out.println("Array ordenado: ");
+    printArray(arr);
+  }
 }
 
 // This code is contributed
